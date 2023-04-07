32ac57d7c026b83fcc1fce765a537677aa266787
Wilson <widacardona1@gmail.com>
2023-04-07 14:07:39 -0500

    Ahora el Hello world tiene una variable

--- src/com/HelloWorld/HelloWorld.java
+++ src/com/HelloWorld/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        String test = "Hello world";
+
+        System.out.println(test);
 
     }
 }
